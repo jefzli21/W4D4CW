f3e3663eb02903ce311a8ac1317b48079b0c009a
AA Student <appacademy@Mini-46.local>
2022-07-07 15:32:14 -0700

    First TDD done

--- lib/tdd.rb
+++ lib/tdd.rb
@@ -21,8 +21,109 @@ class Array
         zeros
     end
 
-    #will transpose the matrix
-    def my_transpose(matrix)
-
+   
+end
+ #will transpose the matrix
+ def my_transpose(matrix)
+    new_arr = []
+    (0...matrix.length).each do |i|
+        temp = []
+        (0...matrix.length).each do |j|
+            temp << matrix[j][i]
+        end
+        new_arr << temp
     end
+    new_arr
+end
+#[day 0 price, day 1 price, .....day n price]
+#buy on the lowest possible day, and sell on the highest possible day
+#we cannot sell until we buy [11, 1000, 10, 100]
+def stock_picker(prices)
+    max = 0
+    days = []
+    (0...prices.length).each do |idx1|
+        (idx1+1...prices.length).each do |idx2|
+            if prices[idx2] - prices[idx1] > max
+                max = prices[idx2] - prices[idx1] 
+                days = [idx1, idx2]
+            end
+        end
+    end
+    days
+end
+
+class Hanoi
+
+    attr_accessor :first, :mid, :last
+    
+    def initialize
+        @first = [3, 2, 1]
+        @mid = []
+        @last = []
+    end
+
+    def get_input
+        while !won?
+            puts 'Enter which stack you want to move from (1 , 2 , 3)'
+            stack_from = gets.chomp.to_i 
+            puts 'Enter which stack you want to move to (1 , 2 , 3)'
+            stack_to = gets.chomp.to_i
+            if stack_from == stack_to
+                raise "Invalid move"
+            end
+            move(stack_from, stack_to)
+            p @first
+            puts
+            p @mid
+            puts
+            p @last
+
+        end
+        puts "You Wont the game"
+    end
+
+    def move(stack_from, stack_to)
+        case stack_from
+        when 1 
+            current_stack = @first
+        when 2
+            current_stack = @mid
+        when 3
+            current_stack = @last
+        end
+
+        case stack_to 
+        when 1 
+            final_stack = @first
+        when 2
+            final_stack = @mid
+        when 3
+            final_stack = @last
+        end
+
+        if self.valid_move?(current_stack, final_stack)
+            final_stack << current_stack.pop
+            final_stack
+        else
+            puts "Muahahaha not valid, try again: "
+        end
+        final_stack
+    end
+
+
+    def won?
+        @last == [3,2,1]
+    end
+
+    def valid_move?(stack1, stack2)
+        current_stack = stack1.dup
+        final_stack = stack2.dup
+        final_stack << current_stack.pop
+        final_stack == final_stack.sort.reverse
+    end
+
+
+    
+    
+    
 end
--- spec/tdd_spec.rb
+++ spec/tdd_spec.rb
@@ -27,11 +27,87 @@ describe Array do
 end
 
 describe '#my_transpose' do 
-    matrix = [[]]
+    matrix = [[0,1,2], [3,4,5], [6,7,8]]
+
     context 'Given a matrix, return its transposed self' do
         it "should transpose the rows and columns of the matrix" do
-
+            expect(my_transpose(matrix)).to eq(matrix.transpose)
         end
+    end
+
+
+end
+
+#[day 0 price, day 1 price, .....day n price]
+#buy on the lowest possible day, and sell on the highest possible day
+#we cannot sell until we buy [11, 1000, 10, 100]
+
+describe "stock_picker" do 
+    context "buy on the lowest possible day, and sell on the highest possible day" do
+        it "Should return a pair of days resulting in larges profit" do 
+            expect(stock_picker([11, 1000, 10, 100])).to eq([0,1])
+            expect(stock_picker([11, 1000, 10, 100,10000,1000000000])).to eq([2,5])
+        end
+    end
+end
+
+
+describe Hanoi do 
+    subject(:tower) {Hanoi.new}
+    describe "#initialize" do 
+        context 'should set 3 arrays as stacks' do 
+            it "should set first stack with 3 pieces from biggest to smallest" do
+                expect(tower.first).to eq([3, 2, 1])
+                expect(tower.mid).to eq([])
+                expect(tower.last).to eq([])
+            end
+        end
+    end
+    
+    # describe "#get_input" do
+    #     context 'gets input from user' do
+    #         it 'does not return nil' do
+    #             expect(tower.get_input).to_not eq(nil) 
+    #         end
+    #     end
+    # end
+
+    describe "#move" do 
+        context 'should move piece from one stack to another' do 
+            it 'should return final stack with new piece' do 
+                expect(tower.move(1,3)).to eq([1])
+                expect(tower.move(1,3)).to eq([1])
+                expect(tower.move(1,3)).to eq([1])
+                expect(tower.move(1,2)).to eq([2])
+                expect(tower.move(3,2)).to eq([2,1])
+            end
+        end
+    end
+
+    describe '#won?' do
+        
+        it 'should return true when last stack is full' do 
+            tower.last = [3,2,1]
+            expect(tower.won?).to eq(true)
+            
+        end
+    end
+
+    describe '#valid_move?' do 
+    #     context "when valid" do 
+    #         it 'should return true' do
+    #             expect(tower.valid_move?).to be(true)
+    #         end
+    #     end
+
+        context "when not valid" do 
+            it 'should return false' do
+                stack2 = [1]
+                stack1 = [3,2]
+                expect(tower.valid_move?(stack1,stack2)).to be(false)
+            end
+        end
+
     end
 
 
